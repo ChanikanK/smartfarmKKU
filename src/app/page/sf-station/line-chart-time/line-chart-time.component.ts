2dfe3518ba7d0b34163542e1e3310eb13768fabe
ChanikanK <k_chanikan@kkumail.com>
2021-03-31 13:34:17 +0700

    line chart color

--- src/app/page/sf-station/line-chart-time/line-chart-time.component.ts
+++ src/app/page/sf-station/line-chart-time/line-chart-time.component.ts
@@ -17,30 +17,16 @@ export class LineChartTimeComponent implements OnInit {
   };
   lineChartLabels: any = [];
   lineChartLegend = true;
-  // lineChartColor: any = [
-  //   {
-  //     backgroundColor: [
-  //       'rgba(30, 169, 224, 0.8)',
-  //       'rgba(255,165,0,0.9)',
-  //       'rgba(139, 136, 136, 0.9)',
-  //       'rgba(255, 161, 181, 0.9)',
-  //       'rgba(255, 102, 0, 0.9)',
-  //     ],
-  //   },
-  // ];
+
+  colours: any = [
+    {
+      backgroundColor:' rgba(140,204,12,0.6)',
+    },
+  ];
   lineChartData: any = [
     {
       data: ['2006', '2007', '2008', '2009', '2010', '2010'],
       label: 'label',
-      colours: [{ // default
-        fillColor: "rgba(0, 108, 112, 1)",
-        "strokeColor": "rgba(207,100,103,1)",
-        "pointColor": "rgba(220,220,220,1)",
-        "pointStrokeColor": "#fff",
-        "pointHighlightFill": "#fff",
-        "pointHighlightStroke": "rgba(151,187,205,0.8)"
-      }]
-
     },
   ];
 
