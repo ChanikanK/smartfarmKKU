e0a3e1d9310338c95133fbab49b357c8a19400c4
ChanikanK <k_chanikan@kkumail.com>
2021-04-01 20:18:51 +0700

    build 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  farmAPILink:  'https://apiweedsmf.kku.ac.th/orion/v2/entities',
+  sthAPILink: 'https://apiweedsmf.kku.ac.th/sth/STH/v1/contextEntities'
 };
